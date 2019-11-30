fc14146b187816e71ce721028e1fccc211f67730
IshkhneliMariami <51286355+IshkhneliMariami@users.noreply.github.com>
2019-11-30 15:43:55 +0400

    Create GenerateInter.java

---
+++ Homework#4/GenerateInter.java
@@ -0,0 +1,8 @@
+public interface GenerateInter {
+
+    public void genAlph();
+    public char genChar();
+    public String genWord(int min, int max);
+    public String genSent(int min, int max);
+    
+}
